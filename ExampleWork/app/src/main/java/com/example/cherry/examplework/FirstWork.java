fe30bb68d9148a731ccae918fe01ea9e80014450
chaitanya <chaitanyakarri963@gmail.com>
2020-06-06 11:18:38 +0530

    updated

---
+++ ExampleWork/app/src/main/java/com/example/cherry/examplework/FirstWork.java
@@ -0,0 +1,22 @@
+package com.example.cherry.examplework;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class FirstWork extends Worker {
+    public FirstWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.i("Chaitu","This is my work");
+        return Result.success();
+    }
+}
---
+++ ExampleWork/app/src/main/java/com/example/cherry/examplework/SecondRequest.java
@@ -0,0 +1,24 @@
+package com.example.cherry.examplework;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class SecondRequest extends Worker {
+    String text;
+    public SecondRequest(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        text = getInputData().getString("key");
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.i("Chaitu",text);
+        return Result.success();
+    }
+}
